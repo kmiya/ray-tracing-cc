4db2555aa493a8c776880304d90322805c4edd75
Kazuki Miyahara <kmiya@outlook.com>
2025-09-08 01:03:15 +0900

    Add ray.hh

---
+++ src/ray.hh
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "vec3.hh"
+
+class Ray {
+ public:
+  Ray() = default;
+
+  // NOLINTNEXTLINE(bugprone-easily-swappable-parameters)
+  Ray(const Point3& origin, const Vec3& direction)
+      : orig_(origin), dir_(direction) {}
+
+  [[nodiscard]] auto Origin() const -> const Point3& { return orig_; }
+  [[nodiscard]] auto Direction() const -> const Vec3& { return dir_; }
+
+  [[nodiscard]] auto At(double t) const -> Point3 { return orig_ + t * dir_; }
+
+ private:
+  Point3 orig_;
+  Vec3 dir_;
+};
