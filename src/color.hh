e75eca7e4cd004466d6fbffee77a75a6c4efb04b
Kazuki Miyahara <kmiya@outlook.com>
2025-09-18 08:53:18 +0900

    9.5 Using Gamma Correction for Accurate Color Intensity

--- src/color.hh
+++ src/color.hh
@@ -7,12 +7,28 @@
 
 using Color = Vec3;
 
+inline auto LinerToGamma(double linear_component) -> double {
+  if (linear_component > 0) {
+    return std::sqrt(linear_component);
+  }
+  return 0.0;
+}
+
 constexpr void WriteColor(std::ostream& out, const Color& pixel_color) {
+  double r = pixel_color.X();
+  double g = pixel_color.Y();
+  double b = pixel_color.Z();
+
+  // Apply a linear to gamma transform for gamma 2
+  r = LinerToGamma(r);
+  g = LinerToGamma(g);
+  b = LinerToGamma(b);
+
   // Translate the [0,1] component values to the byte range [0,255].
   static const Interval kIntensity(0.000, 0.999);
-  const int r_byte = static_cast<int>(256 * kIntensity.Clamp(pixel_color.X()));
-  const int g_byte = static_cast<int>(256 * kIntensity.Clamp(pixel_color.Y()));
-  const int b_byte = static_cast<int>(256 * kIntensity.Clamp(pixel_color.Z()));
+  const int r_byte = static_cast<int>(256 * kIntensity.Clamp(r));
+  const int g_byte = static_cast<int>(256 * kIntensity.Clamp(g));
+  const int b_byte = static_cast<int>(256 * kIntensity.Clamp(b));
 
   // Write out the pixel color components.
   out << r_byte << ' ' << g_byte << ' ' << b_byte << '\n';
